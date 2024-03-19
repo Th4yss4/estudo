7c8ff7d4739345c72b9b4fee57ac8ebd3b4cbc40
Thayssa Fernanda <thayssananda@ufpi.edu.br>
2024-03-19 20:42:34 -0300

    Create test1.js

---
+++ Teste2/test1.js
@@ -0,0 +1,15 @@
+// Addition
+let sum = 2 + 3;
+console.log("Sum:", sum);
+
+// Subtraction
+let difference = 5 - 2;
+console.log("Difference:", difference);
+
+// Multiplication
+let product = 4 * 3;
+console.log("Product:", product);
+
+// Division
+let quotient = 10 / 2;
+console.log("Quotient:", quotient);
